8870faa82f52423bfef00abe648c9b4f018678ad
Omii1908 <omi844198@gmail.com>
2025-11-05 12:54:01 +0530

    Configured github actions for deployment and updated dependencies

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/Reflex_Web/', // Set this to your repository name
+    plugins: [react()],
+})
