017bd92f056ac0fe932f6ccc8d9e2a9d9cdf8928
Omii1908 <omi844198@gmail.com>
2025-11-05 14:00:30 +0530

    feat: configure GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Reflex_Web/', // Set this to your repository name
     plugins: [react()],
+    base: '/Reflex_Web/',
 })
